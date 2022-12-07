d260ed09cbfc8190cfc7ef61bdb921898a149f15
Lotacodic <Ngenejoshua05@gmail.com>
2022-12-07 06:03:17 +0100

    modified index.html and style.css files, and javascript eventListener

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-// somethin in here
-// something in here
+const wrapper = document.querySelector(".sliderWrapper");
+const menuItems = document.querySelectorAll(".menuItem");
+
+menuItems.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        wrapper.style.transform = `translateX(${-100 * index}vw)`;
+    });
+});
