ce046073760d665b2fbf03c89e600cdaf5c77336
Lotacodic <Ngenejoshua05@gmail.com>
2022-12-11 16:05:03 +0100

    Add functionalities to modal

--- script.js
+++ script.js
@@ -124,3 +124,15 @@ currentProductSizes.forEach((size, index) => {
         size.style.color = "white"
     })
 })
+
+const productButton = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+productButton.addEventListener("click", () => [
+    payment.style.display = "flex"
+])
+
+close.addEventListener("click", () => [
+    payment.style.display = "none"
+])
